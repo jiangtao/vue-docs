b03535f1dae701c2b9ab6372fd5279520856bf54
Jerret321 <321jiangtao@gmail.com>
2017-01-14 19:26:29 +0800

    Update Doc

--- build/ghpage.sh
+++ build/ghpage.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-git checkout gh-pages
 cp -r dist/css .
 cp -r dist/js .
 cp dist/index.html index.htm
-
 git add js css index.htm
-# git commit -m "Update Doc"
-# git push -u origin gh-pages
+git commit -m "Update Doc"
+git push -u origin gh-pages
 
-# git checkout master
-# rm -fr css js index.htm
+git checkout master
+rm -fr css js index.htm
